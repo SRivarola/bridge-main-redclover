4d32f4e0a273d471df546cbdd2e6f9129e1c896d
Santiago Rivarola <srivarola@gmail.com>
2024-09-25 19:36:08 -0300

    arreglo headers sku info

--- api/previsora/getSkuInfo.js
+++ api/previsora/getSkuInfo.js
@@ -1,26 +1,24 @@
 import fetch from "node-fetch";
 
 export default async function handler(request, response) {
-    const { skuId } = request.query;
-    try {
-        const res = await fetch(
-          `https://previsoraarg.vtexpayments.com.br/api/catalog/pvt/stockkeepingunit/${skuId}`,
-          {
-            method: "GET",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json; charset=utf-8",
-              "X-VTEX-API-AppKey": process.env.PREVISORA_VTEX_APPKEY,
-              "X-VTEX-API-AppToken": process.env.PREVISORA_VTEX_APPTOKEN,
-            },
-          }
-        );
+  const { skuId } = request.query;
+  try {
+    const res = await fetch(
+      `https://previsoraarg.vtexpayments.com.br/api/catalog/pvt/stockkeepingunit/${skuId}`,
+      {
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json; charset=utf-8",
+          "X-VTEX-API-AppKey": process.env.PREVISORA_VTEX_APPKEY,
+          "X-VTEX-API-AppToken": process.env.PREVISORA_VTEX_APPTOKEN,
+        },
+      }
+    );
 
-        const jsonres = await res.json();
-
-        response.status(200).json(jsonres);
-
-    } catch (error) {
-        response.status(500).json(error);
-    }
+    const jsonres = await res.json();
+    response.status(200).json(jsonres);
+  } catch (error) {
+    response.status(500).json(error);
+  }
 }
