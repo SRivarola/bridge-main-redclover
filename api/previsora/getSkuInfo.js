1b2c696df806795b48d5aa2573fe54b4abfaa4a4
Santiago Rivarola <srivarola@gmail.com>
2024-09-25 19:47:45 -0300

    change url getskuinfo

--- api/previsora/getSkuInfo.js
+++ api/previsora/getSkuInfo.js
@@ -2,21 +2,19 @@ import fetch from "node-fetch";
 
 export default async function handler(request, response) {
     const { skuId } = request.query;
-    console.log("sku: ",skuId)
     try {
       const res = await fetch(
-        `https://previsoraarg.vtexpayments.com.br/api/catalog/pvt/stockkeepingunit/${skuId}`,
+        `https://previsoraarg.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit/${skuId}`,
         {
           method: "GET",
           headers: {
-            "Accept": "application/json",
+            Accept: "application/json",
             "Content-Type": "application/json; charset=utf-8",
             "X-VTEX-API-AppKey": process.env.PREVISORA_VTEX_APPKEY,
             "X-VTEX-API-AppToken": process.env.PREVISORA_VTEX_APPTOKEN,
           },
         }
       );
-      console.log("respuesta: ", res);
       const jsonres = await res.json();
       response.status(200).json(jsonres);
     } catch (error) {
